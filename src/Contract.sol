cb873fffd50584ac0c82d2f0185621c9fd0ff0c2
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-04-22 15:11:10 -0400

    set new comment

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,26 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract Contract {}
+//    __       ___ ___
+//  /'_ `\   /' __` __`\
+// /\ \L\ \  /\ \/\ \/\ \
+// \ \____ \ \ \_\ \_\ \_\
+//  \/___L\ \ \/_/\/_/\/_/
+//    /\____/
+//    \_/__/
+
+// Generated with https://patorjk.com/software/taag/#p=display&h=0&f=Standard&t=github.com%2Frjected
+//
+//           _   _     _               _                                         __           _                 _                _
+//    __ _  (_) | |_  | |__    _   _  | |__         ___    ___    _ __ ___      / /  _ __    (_)   ___    ___  | |_    ___    __| |
+//   / _` | | | | __| | '_ \  | | | | | '_ \       / __|  / _ \  | '_ ` _ \    / /  | '__|   | |  / _ \  / __| | __|  / _ \  / _` |
+//  | (_| | | | | |_  | | | | | |_| | | |_) |  _  | (__  | (_) | | | | | | |  / /   | |      | | |  __/ | (__  | |_  |  __/ | (_| |
+//   \__, | |_|  \__| |_| |_|  \__,_| |_.__/  (_)  \___|  \___/  |_| |_| |_| /_/    |_|     _/ |  \___|  \___|  \__|  \___|  \__,_|
+//   |___/                                                                                 |__/
+//
+
+contract GreeterV1 {
+    function greet() public pure returns (string memory) {
+        return "gm!";
+    }
+}
